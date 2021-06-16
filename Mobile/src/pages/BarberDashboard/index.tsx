99545381152dfb0f72efb12bc237cb518bd47656
Matheus Gonçalves <matheusluiz.barbosa@outlook.com>
2021-06-16 13:51:23 -0300

    Created button of next appointment

--- Mobile/src/pages/BarberDashboard/index.tsx
+++ Mobile/src/pages/BarberDashboard/index.tsx
@@ -1,8 +1,15 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable import/no-duplicates */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { useNavigation } from '@react-navigation/native';
-import React, { useCallback } from 'react';
+import { format, isAfter, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Avatar } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/AntDesign';
 import { useAuth } from '../../hooks/auth';
+
+import api from '../../services/api';
 
 import {
   Container,
@@ -12,15 +19,83 @@ import {
   UserAvatar,
   UserName,
   AppointmentContainer,
+  DateText,
   Title,
+  TextAppointment,
+  AppointmentInfo,
+  AppointmentName,
+  AppointmentAvatar,
+  AppointmentService,
+  AppointmentDate,
+  NextAppointment,
   MorningAppointments,
   AfternoonAppointments,
 } from './styles';
+
+interface Appointment {
+  id: string;
+  service: string;
+  date: string;
+  hourFormatted: string;
+  user: {
+    name: string;
+    avatar_url: string;
+  };
+}
 
 const BarberDashboard: React.FC = () => {
   const { navigate } = useNavigation();
 
   const { user, signOut } = useAuth();
+
+  const [appointments, setAppointments] = useState<Appointment[]>([]);
+
+  const [today] = useState(new Date());
+
+  useEffect(() => {
+    api
+      .get<Appointment[]>('/appointments/me', {
+        params: {
+          year: today.getFullYear(),
+          month: today.getMonth() + 1,
+          day: today.getDate(),
+        },
+      })
+      .then((response) => {
+        const appointmentsFormatted = response.data.map((appointment) => {
+          return {
+            ...appointment,
+            hourFormatted: format(parseISO(appointment.date), 'HH:mm'),
+          };
+        });
+        setAppointments(appointmentsFormatted);
+      });
+  }, [today]);
+
+  const todayDateAsText = useMemo(() => {
+    return format(today, "'Dia' dd 'de' MMMM 'de' yyyy", {
+      locale: ptBR,
+    });
+  }, [today]);
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter((appointment) => {
+      return parseISO(appointment.date).getHours() < 12;
+    });
+  }, [appointments]);
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter((appointment) => {
+      return parseISO(appointment.date).getHours() >= 12;
+    });
+  }, [appointments]);
+
+  const nextAppointment = useMemo(() => {
+    return appointments.find((appointment) =>
+      isAfter(parseISO(appointment.date), new Date())
+    );
+  }, [appointments]);
+
   const navigateToProfile = useCallback(() => {
     navigate('Profile');
   }, [navigate]);
@@ -57,14 +132,47 @@ const BarberDashboard: React.FC = () => {
       </Header>
 
       <AppointmentContainer>
-        <Title>Agendamentos de hoje</Title>
+        <Title>Agendamentos</Title>
+        <DateText>{todayDateAsText}</DateText>
+
+        <TextAppointment>Próximo agendamento</TextAppointment>
+        {nextAppointment ? (
+          <NextAppointment>
+            <AppointmentInfo>
+              {nextAppointment.user.avatar_url ? (
+                <AppointmentAvatar
+                  source={{ uri: nextAppointment.user.avatar_url }}
+                />
+              ) : (
+                <Avatar.Text
+                  color="#fff"
+                  style={{ backgroundColor: '#ff9000', left: -8 }}
+                  label={nextAppointment.user.name[0]}
+                />
+              )}
+              <AppointmentName numberOfLines={2}>
+                Cliente: {nextAppointment.user.name}
+              </AppointmentName>
+              <AppointmentService numberOfLines={2}>
+                Serviço: {nextAppointment.service}
+              </AppointmentService>
+              <AppointmentDate>
+                Horário: {nextAppointment.hourFormatted}
+              </AppointmentDate>
+            </AppointmentInfo>
+          </NextAppointment>
+        ) : (
+          <NextAppointment>
+            <TextAppointment>Não há agendamentos para hoje</TextAppointment>
+          </NextAppointment>
+        )}
 
         <MorningAppointments>
-          <Title>Serviços da manhã</Title>
+          <Title>Agendamentos da manhã :</Title>
         </MorningAppointments>
 
         <AfternoonAppointments>
-          <Title>Serviços da manhã</Title>
+          <Title>Agendamentos da tarde :</Title>
         </AfternoonAppointments>
       </AppointmentContainer>
     </Container>
--- Mobile/src/pages/BarberDashboard/styles.ts
+++ Mobile/src/pages/BarberDashboard/styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RectButton } from 'react-native-gesture-handler';
 
 export const Container = styled.View`
   flex: 1;
@@ -38,12 +39,69 @@ export const UserAvatar = styled.Image`
   border-radius: 28px;
 `;
 
-export const AppointmentContainer = styled.View``;
+export const AppointmentContainer = styled.View`
+  flex: 1;
+`;
 
 export const Title = styled.Text`
   padding: 20px;
-  color: #fff;
+  color: #ff9000;
   font-size: 24px;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  font-weight: bold;
+`;
+
+export const DateText = styled.Text`
+  padding: 0 20px;
+  color: #fff;
+  font-size: 16px;
+  margin-bottom: 15px;
+`;
+
+export const TextAppointment = styled.Text`
+  padding: 20px 30px;
+  color: #fff;
+  font-size: 16px;
+`;
+
+export const NextAppointment = styled(RectButton)`
+  width: 90%;
+  margin: 0 auto;
+  background: #3b3b47;
+  opacity: 0.9;
+  border-radius: 10px;
+`;
+
+export const AppointmentInfo = styled.View`
+  padding: 20px 30px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  right: 10px;
+`;
+
+export const AppointmentAvatar = styled.Image`
+  width: 60px;
+`;
+
+export const AppointmentName = styled.Text`
+  font-size: 16px;
+  width: 80px;
+  color: #fff;
+`;
+
+export const AppointmentService = styled.Text`
+  margin: 0 5px;
+  font-size: 16px;
+  width: 80px;
+  color: #fff;
+`;
+
+export const AppointmentDate = styled.Text`
+  font-size: 16px;
+  width: 80px;
+  color: #fff;
 `;
 
 export const MorningAppointments = styled.View``;
